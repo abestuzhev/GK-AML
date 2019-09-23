6b2f1a9006678a881fea7c09ccddf45296fcee43
Aleksey <aleksey.bestuzhev@gmail.com>
2019-09-23 16:28:26 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -3,4 +3,11 @@ $(function(){
         dots: true,
         infinite: true,
     });
+
+    $('.reviews-list').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
 });
