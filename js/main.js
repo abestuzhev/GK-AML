74b915750f3327197dbfef9f72f1e44ae7c5cee6
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2019-09-25 00:01:55 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     $('.slider').slick({
         dots: true,
         infinite: true,
@@ -6,9 +6,55 @@ $(function(){
 
     $('.reviews-list').slick({
         dots: true,
-        arrows:false,
+        arrows: false,
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 4
     });
+
+    $(document).on('click', '.header-basket__link', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $('.popup-basket').toggleClass('is-show');
+
+    });
+
+    $(document).on('click', '.popup-basket__close', function (e) {
+        e.preventDefault();
+        $('.header-basket__link').removeClass('active');
+        $('.popup-basket').removeClass('is-show');
+
+    });
+
+
+    $('#header-search').on('focus', function(){
+        $('.popup-search').addClass('is-show');
+    });
+    $('#header-search').blur(function(){
+        $('.popup-search').removeClass('is-show');
+    });
+
+
+
+    $(document).mousedown(function (e) { // событие клика по веб-документу
+        // var div = $('.popup');
+        // тут указываем ID элемента
+        // var div2 = $('.popup-mini');
+
+
+        function hideOutZone(elem){
+            var div = $(elem);
+            if (!div.is(e.target) && div.has(e.target).length === 0){
+
+                $('.header-basket__link').removeClass('active');
+                $('.popup-basket').removeClass('is-show');
+            }
+        }
+        if (e.which === 1) {
+            hideOutZone('.popup-basket');
+
+        }
+    });
+
+
 });
